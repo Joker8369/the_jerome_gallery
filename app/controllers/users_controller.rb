8bc03069e6300047c2507cff38827e9ea9085974
jonathan kerbrat <jonathan.kerbrat@hotmail.com>
2021-10-30 15:54:01 +0200

    fix cl user and update profile

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+    before_action :find_user, only: %i[edit update]
+  def edit
+  end
+
+  def update
+    @user.update(users_params)
+    redirect_to root_path
+  end
+
+  def show
+  end
+
+  private
+
+  def users_params
+    params.require(:user).permit(:last_name, :first_name,:address, :phone, :email, :story, :photo)
+  end
+
+  def find_user
+    @user = User.find(params[:id])
+  end
+end
